2bb73083cb5c1dca2f7101900a58367be1d4e825
RKK <18715791835@163.com>
2020-08-10 14:27:30 +0800

    "这一次提交了BaseController文件"

---
+++ java_spring_boot/src/main/java/com/sfac/java_spring_boot/models/test/controller/BaseController.java
@@ -0,0 +1,10 @@
+package com.sfac.java_spring_boot.models.test.controller;
+
+/*
+@author:Lenovo
+@program:java_spring_boot
+@date:2020-08-10 14:24:40
+@description:
+*/
+public class BaseController {
+}
